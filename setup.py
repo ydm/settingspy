7bcea9c98ae09ae22e61c6a25970715c04991147
Yordan Miladinov <yordan@4web.bg>
2014-10-15 16:39:35 +0300

    Variable catalog is now renamed "Setting catalog" in setup.py's long_description

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ This module provides a `spy` object that resolves an attribute by
 searching sequentially in following places:
 
 0. manually set settings
-1. variable catalog
+1. settings catalog
 2. user-provided settings module
 3. manually set fallbacks
 
@@ -17,18 +17,19 @@ Examples:
 from settingspy import spy
 spy['this_is_int'] = 123
 spy['this_is_str'] = 'string'
+print(spy.this_is_int, spy.this_is_str)
 
 
-1. Variable catalog
+1. Settings catalog
 
-Inside the directory specified by the SETTINGSPY_VARIABLE_CATALOG
-environment variable, a file named `something` may exist with the
-desired value.  File contents are restricted to booleans, integers,
-floats, strings.  They are parsed as if eval()ed, so strings should be
-wrapped in parentheses.
+Inside the directory specified by the SETTINGSPY_CATALOG environment
+variable, a file named `something` may exist with the desired value.
+File contents are restricted to booleans, integers, floats, strings.
+They are parsed as if eval()ed, so strings should be wrapped in
+parentheses.
 
-$ echo 123 > "$SETTINGSPY_VARIABLE_CATALOG/this_is_int"
-$ echo "'string'" > "$SETTINGSPY_VARIABLE_CATALOG/this_is_str"
+$ echo 123 > "$SETTINGSPY_CATALOG/this_is_int"
+$ echo "'string'" > "$SETTINGSPY_CATALOG/this_is_str"
 
 
 2. User provided settings module
@@ -37,23 +38,20 @@ in file mysettings.py:
 this_is_int = 123
 this_is_str = 'string'
 
-import os; os.environ['SETTINGSPY_SETTINGS_MODULE'] = 'mysettings'
+import os; os.environ['SETTINGSPY_MODULE'] = 'mysettings'
 from settingspy import spy; print(spy.this_is_int, spy.this_is_str)
 
 
-3. Manually set fallbacks -- in case everything else fails
+3. Manually set fallbacks
+
+In case a setting attribute isn't defined anywhere else.
 
 from settingspy import spy
 spy.setfallback('this_is_int', 123)
 spy.setfallback('this_is_str', 'string')
 
-
-This module provides a `spy` object that resolves a setting attribute by
-searching sequentially in following places: manually set settings,
-directory variables (also known as variable catalog), user-provided
-settings module, manually set fallback settings.
-
-See the source code at GitHub at https://github.com/ydm/settingspy
+For more details see the source code at
+https://github.com/ydm/settingspy
 '''
 
 
